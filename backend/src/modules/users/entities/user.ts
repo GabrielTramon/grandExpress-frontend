49080c8d19c85cf1b05bb1bdc70c4bb9360a8662
Gabriel Tramontin <133909024+GabrielTramon@users.noreply.github.com>
2025-09-21 16:37:47 -0300

    Delete backend directory

--- backend/src/modules/users/entities/user.ts
+++
@@ -1,3 +0,0 @@
-import { Users } from "@prisma/client";
-
-export interface UserEntity extends Users {}
--- backend/src/modules/users/repositories/IUserRepository.ts
+++
@@ -1,6 +0,0 @@
-import { CreateUserDTO } from "modules/users/dtos/createDTO";
-import { UserEntity } from "modules/users/entities/user";
-
-export interface IUserRepository {
-    create(data: CreateUserDTO, createdById: string): Promise<UserEntity>;
-}
